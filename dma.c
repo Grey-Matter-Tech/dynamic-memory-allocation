0dfe4064c3f231f63a8e6383cef456f0b8f4676b
HarshDave <harshdave002@gmail.com>
2022-08-20 13:13:16 +0530

    Code cleanup
    - Replaced datatypes with user defined datatypes

--- dma.c
+++ dma.c
@@ -1,45 +1,66 @@
 /*
- * dma.c
+ * Copyright (c) 2022, Harsh Dave.
+ * All rights reserved.
  *
- *  Created on: **-***-20**
- *      Author: HarshDave
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *
+ */
+
+/**
+ * \file
+ *         A very simple implementation of dynamic memory allocation APIs
+ * \author
+ *         Harsh Dave <HarshDave-Sithlord>
  */
 #include "dma.h"
 #include "utils.h"
 
-//Heap memory HEAD. chop it off.
+void hheap_show(void);
+void hheap_maintenance(void * free_ptr);
 struct heap_memory *hheap = NULL;
 
-void hheap_show(void)
+/**
+ * find_fit
+ * ARGS:size(size of memory chunk to be allocated)
+ * Return value: void *(returns starting address of memory chunk available)
+ * Description: Traverse through hheap memory, looks for memory chunk which
+ * is large enough to hold data of given size and finally returns its address.
+ */
+static void * find_fit(uint32_t size)
 {
-	unsigned int *header = HEAP_LOW_END;
-	unsigned char *buffer = (unsigned char *)(header + 1);
-	unsigned int upto = 0;
-	while(header < HEAP_HIGH_END && ((*header & 1) == 1))
+	uint8_t *start = (uint8_t *)HEAP_LOW_END;
+	while(start < (uint8_t *)HEAP_HIGH_END)
 	{
+		if( ( (!(*(uint32_t *)start & 1)) && (*(uint32_t *)start > size) ) )
 		{
-			printf("HEADER :: [%p][%d]\n", header, *header);
-			printf("Buffer :: \n");
-			upto = (((*header) & ~(1)) - HEADER_SIZE);
-			buffer = (unsigned char *) (header + 1);
-			if(upto < 0)
-			{
-				return;
-			}
-
-			for(int i =0; i< upto; i++)
-			{
-				if(i%100==0)
-					printf("\n");
-				printf("%c ", buffer[i]);
-			}
-			printf("\n");
+			return (void *)start;
 		}
-		buffer = (unsigned char *)header;
-		buffer +=  (*header & ~(1));
-		header = (unsigned int *)buffer;
+		start += ((*(uint32_t *)start) & ~(1));
 	}
-	printf("\n");
+	return NULL;
 }
 
 /**
@@ -51,9 +72,9 @@ void hheap_show(void)
  * Sets up meta information regarding heap such as remaining memory,
  * total memory and the very first header in heap memory.
  */
-unsigned char hheap_init(void)
+bool_t hheap_init(void)
 {
-	unsigned char ret = FAIL;
+	bool_t ret = FAIL;
 	/**
 	 * Allocates the memory of size specified by macro HEAP_SIZE + heap meta data.
 	 */
@@ -77,102 +98,44 @@ unsigned char hheap_init(void)
 	return ret;
 }
 
-/**
- * hheap_stats
- * ARGS:none
- * Return value: none
- * Description: Dumps hheap memory's active blocks
- * and its corresponding size.
- */
-void hheap_stats(void)
+void *hheap_alloc(uint32_t size)
 {
-	unsigned int *ptr = (unsigned int *)HEAP_LOW_END;
-	while(ptr < (unsigned int *)HEAP_HIGH_END)
+	void *header = NULL;
+	uint32_t total_size = ALIGN(size + HEADER_SIZE);
+
+#if (DEBUG == HEAP_MEM_SIZE_DEBUG) || (DEBUG == HEAP_DEBUG_ALL)
+	printf("hmalloc :: total size needed is :: %d\n", total_size);
+#endif
+	header = find_fit(total_size);
+	if(header)
 	{
-		if ((*ptr & 1))
-		{
-			/**
-			 * NOTE: Since the ptr is unsigned int type of
-			 * pointer, it does memory jummp by multiple of
-			 * sizeof(unsigned int). So to correct the offset
-			 * it needs to be divided back by sizeof(unsigned int).
-			 * If ptr were to be unsigned char or void pointer, no such
-			 * division would have been necessary.
-			 */
-			ptr += (*ptr & ~(1))/sizeof(unsigned int);
-		}
-		else
-		{
-			break;
-		}
+		*(uint32_t *)(header) = (uint32_t)( total_size | 1);
+		/**
+		 * update heap stats. Since a chunk is allocated
+		 * we need the book keeping for available memory
+		 */
+		UPDATE_REM_MEM(total_size);
+		*(uint32_t *)(header + total_size ) = (hheap->rem_mem);
+
+#if (DEBUG == HEAP_ADDRESS_DEBUG) || (DEBUG == HEAP_DEBUG_ALL)
+		printf("hmalloc :: Setting header %p[%d]\n", header, *(unsigned int*)header);
+#endif
+		header += HEADER_SIZE;
 	}
+	else
+	{
+		printf("hmalloc :: Error finding chunk\n");
+	}
+
+	return (void *)(header);
 }
 
-/**
- * find_fit
- * ARGS:size(size of memory chunk to be allocated)
- * Return value: void *(returns starting address of memory chunk available)
- * Description: Traverse through hheap memory, looks for memory chunk which
- * is large enough to hold data of given size and finally returns its address.
- */
-static void * find_fit(unsigned int size)
+bool_t hheap_free(void *addr)
 {
-	unsigned char *start = (unsigned char *)HEAP_LOW_END;
-	while(start < (unsigned char *)HEAP_HIGH_END)
-	{
-		if(( (!(*(unsigned int *)start & 1)) && (*(unsigned int *)start > size) ) /*|| (!(*start & 1))*/)
-		{
-			return (void *)start;
-		}
-		start += ((*(unsigned int *)start) & ~(1));
-	}
-	return NULL;
-}
-
-
-void hheap_maintenance(void * free_ptr)
-{
-	hheap_show();
-	unsigned int *ptr = (free_ptr + *(unsigned int *)(free_ptr));
-	unsigned char *nptr = (unsigned char *)free_ptr;
-	nptr += *((unsigned int *)free_ptr);
-	unsigned char *src = free_ptr;
-	unsigned long high_end = 0;
-	unsigned int count = 0;
-	while(ptr < HEAP_HIGH_END)
-	{
-		if ((*ptr & 1))
-		{
-			count++;
-			ptr += (*ptr & ~(1))/sizeof(unsigned int);
-		}
-		else
-		{
-			break;
-		}
-	}
-
-	while(count > 0)
-	{
-		high_end = (unsigned long) ((nptr + (*(unsigned int *)nptr & ~(1))) + HEADER_SIZE);
-		while((unsigned long)nptr < high_end)
-		{
-			*src++ = *nptr;
-			*nptr ^= *nptr;
-			nptr++;
-		}
-		count--;
-	}
-
-	hheap_show();
-}
-
-unsigned char hheap_free(void *addr)
-{
-	unsigned char ret = FAIL;
+	bool_t ret = FAIL;
 	if(addr)
 	{
-		unsigned int *header = (addr - HEADER_SIZE);
+		uint32_t *header = (addr - HEADER_SIZE);
 
 #if (DEBUG == HEAP_DEBUG_ALL) || (DEBUG == HEAP_ADDRESS_DEBUG)
 		printf("hfree :: Checking address %p and %p -- [%p][%p]\n", header, addr, HEAP_LOW_END, HEAP_HIGH_END);
@@ -198,44 +161,119 @@ unsigned char hheap_free(void *addr)
 	return ret;
 }
 
-
-void *hheap_alloc(unsigned int size)
+void hheap_flush(void)
 {
-	void *header = NULL;
-	unsigned int total_size = ALIGN(size + HEADER_SIZE);
-
-#if (DEBUG == HEAP_MEM_SIZE_DEBUG) || (DEBUG == HEAP_DEBUG_ALL)
-	printf("hmalloc :: total size needed is :: %d\n", total_size);
-#endif
-	header = find_fit(total_size);
-	if(header)
-	{
-		*(unsigned int *)(header) = (unsigned int)( total_size | 1);
-		/**
-		 * update heap stats. Since a chunk is allocated
-		 * we need the book keeping for available memory
-		 */
-		UPDATE_REM_MEM(total_size);
-		*(unsigned int *)(header + total_size ) = (hheap->rem_mem);
-
-#if (DEBUG == HEAP_ADDRESS_DEBUG) || (DEBUG == HEAP_DEBUG_ALL)
-		printf("hmalloc :: Setting header %p[%d]\n", header, *(unsigned int*)header);
-#endif
-		header += HEADER_SIZE;
-	}
-	else
-	{
-		printf("hmalloc :: Error finding chunk\n");
-	}
-
-	return (void *)(header);
+	memset(hheap->heap, 0, HEAP_SIZE);
+	hheap->heap[0] = HEAP_SIZE;
 }
+
+void hheap_maintenance(void * free_ptr)
+{
+	hheap_show();
+	uint32_t *ptr = (free_ptr + *(uint32_t *)(free_ptr));
+	uint8_t *nptr = (uint8_t *)free_ptr;
+	nptr += *((uint32_t *)free_ptr);
+	uint8_t *src = free_ptr;
+	uint64_t high_end = 0;
+	uint32_t count = 0;
+	while(ptr < HEAP_HIGH_END)
+	{
+		if ((*ptr & 1))
+		{
+			count++;
+			ptr += (*ptr & ~(1))/sizeof(uint32_t);
+		}
+		else
+		{
+			break;
+		}
+	}
+
+	while(count > 0)
+	{
+		high_end = (uint64_t) ((nptr + (*(uint32_t *)nptr & ~(1))) + HEADER_SIZE);
+		while((uint64_t)nptr < high_end)
+		{
+			*src++ = *nptr;
+			*nptr ^= *nptr;
+			nptr++;
+		}
+		count--;
+	}
+
+	hheap_show();
+}
+
+/**
+ * hheap_stats
+ * ARGS:none
+ * Return value: none
+ * Description: Dumps hheap memory's active blocks
+ * and its corresponding size.
+ */
+void hheap_stats(void)
+{
+	uint32_t *ptr = (uint32_t *)HEAP_LOW_END;
+	while(ptr < (uint32_t *)HEAP_HIGH_END)
+	{
+		if ((*ptr & 1))
+		{
+			/**
+			 * NOTE: Since the ptr is unsigned int type of
+			 * pointer, it does memory jummp by multiple of
+			 * sizeof(unsigned int). So to correct the offset
+			 * it needs to be divided back by sizeof(unsigned int).
+			 * If ptr were to be unsigned char or void pointer, no such
+			 * division would have been necessary.
+			 */
+			ptr += (*ptr & ~(1))/sizeof(uint32_t);
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+void hheap_show(void)
+{
+	uint32_t *header = HEAP_LOW_END;
+	uint8_t *buffer = (uint8_t *)(header + 1);
+	uint32_t upto = 0;
+	while(header < HEAP_HIGH_END && ((*header & 1) == 1))
+	{
+		{
+			printf("HEADER :: [%p][%d]\n", header, *header);
+			printf("Buffer :: \n");
+			upto = (((*header) & ~(1)) - HEADER_SIZE);
+			buffer = (uint8_t *) (header + 1);
+			if(upto < 0)
+			{
+				return;
+			}
+
+			for(int i =0; i< upto; i++)
+			{
+				if(i%100==0)
+					printf("\n");
+				printf("%c ", buffer[i]);
+			}
+			printf("\n");
+		}
+		buffer = (uint8_t *)header;
+		buffer +=  (*header & ~(1));
+		header = (uint32_t *)buffer;
+	}
+	printf("\n");
+}
+
 
 struct hheap_driver driver_beta = {
 	.heap = &hheap,
 	.init_heap = hheap_init,
 	.heap_alloc = hheap_alloc,
 	.heap_free = hheap_free,
+	.heap_flush = hheap_flush,
 	.heap_maintenance = hheap_maintenance,
 	.heap_statistics = hheap_stats,
 };
--- sample_application.c
+++ sample_application.c
@@ -1,8 +1,39 @@
 /*
- * sample_application.c
+ * Copyright (c) 2022, Harsh Dave.
+ * All rights reserved.
  *
- *  Created on: **-***-20**
- *      Author: HarshDave
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *
+ */
+
+/**
+ * \file
+ *         A very simple application to validate basic functionality of hheap memory.
+ * \author
+ *         Harsh Dave <HarshDave-Sithlord>
  */
 
 #include "dma.h"
@@ -10,7 +41,7 @@
 
 void main(void)
 {
-	if(driver_beta.init_heap() == OK)
+	if(HEAP.init_heap() == OK)
 	{
 		int i = 0, size = 0;
 		unsigned int *ptr = NULL, *sptr = NULL;
@@ -19,12 +50,13 @@ void main(void)
 		{
 			if( i % 4 == 0)
 			{
-				driver_beta.heap_free(sptr);
+				HEAP.heap_free(sptr);
+				HEAP.heap_flush();
 			}
 			else
 			{
 				size = rand() % 569;
-				ptr = (unsigned int *)driver_beta.heap_alloc(size * sizeof(int));
+				ptr = (unsigned int *)HEAP.heap_alloc(size * sizeof(int));
 				if(ptr)
 				{
 					if(i % catch == 0)
@@ -32,20 +64,20 @@ void main(void)
 						sptr = ptr;
 					}
 #if (DEBUG==APP_DEBUG) || (DEBUG==HEAP_DEBUG_ALL)
-					memset(ptr, ('A' + (rand() % 26)),size * sizeof(int));
+					memset(ptr, ('A' + (rand() % 26)),size /** sizeof(int)*/);
 					printf("Allocated buffer at address :: %p[%d]\n", ptr, size);
 #endif
 				}
 				else
 				{
 #if (DEBUG==APP_DEBUG) || (DEBUG==HEAP_DEBUG_ALL)
-					printf("It is FAILed!!\n");
+					printf("Failed!!\n");
 #endif
 				}
 			}
 			i++;
 		}
 		printf("DUMPING STATS###########################\n");
-		driver_beta.heap_statistics();
+		HEAP.heap_statistics();
 	}
 }
